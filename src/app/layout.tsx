bac147c5ff82db31555793e88e36835b214e68d4
yatochka-dev <philipchef13@gmail.com>
2025-07-26 15:12:21 +0300

    Improve SEO drastically

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,21 @@ import { Geist } from "next/font/google";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://keyvaro.com"),
   title: "KeyVaro",
   description: "Unlock your next stay with a smartphone.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    title: "KeyVaro",
+    description: "Unlock your next stay with a smartphone.",
+    images: [{ url: "/preview.png" }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "KeyVaro",
+    description: "Unlock your next stay with a smartphone.",
+    images: [{ url: "/preview.png" }],
+  },
 };
 
 const geist = Geist({
