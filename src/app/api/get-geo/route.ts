9aa21ac50bab112ee0b067fa5ed2558bc15ab8e6
yatochka-dev <philipchef13@gmail.com>
2025-08-01 20:11:08 +0300

    feat: add location tracking

--- src/app/api/get-geo/route.ts
+++ src/app/api/get-geo/route.ts
@@ -2,6 +2,7 @@ import { geolocation } from "@vercel/functions";
 
 export function GET(request: Request) {
   const { country } = geolocation(request);
+  const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
 
   if (!country) {
     return new Response(`{}`, {
@@ -12,7 +13,7 @@ export function GET(request: Request) {
     });
   }
 
-  return new Response(JSON.stringify({ country }), {
+  return new Response(JSON.stringify({ country: regionNames.of(country) }), {
     headers: { "content-type": "application/json" },
   });
 }
